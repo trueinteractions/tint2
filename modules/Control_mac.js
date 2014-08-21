355df2eb4acf792f73409b26260493367db4aaf9
Trevor Linton <trevor.linton@gmail.com>
2014-08-21 10:39:03 -0600

    indent!!

--- modules/Control_mac.js
+++ modules/Control_mac.js
@@ -38,103 +38,103 @@ module.exports = (function() {
     }.bind(this));
     //TODO: Does not work.
     nativeViewExt.addMethod('mouseUp:',[$.void,['@',$.selector,'@']], function(self, cmd, events) {
-    	self.super('mouseUp',events);
-    	try { 
-    		this.fireEvent('mouseUp');
-    	} catch(e) { 
-    		console.log(e.message);
-    		console.log(e.stack);
-    		process.exit(1);
-    	}
+      self.super('mouseUp',events);
+      try { 
+        this.fireEvent('mouseUp');
+      } catch(e) { 
+        console.log(e.message);
+        console.log(e.stack);
+        process.exit(1);
+      }
     }.bind(this));
     //TODO: Does not work.
     nativeViewExt.addMethod('mouseEntered:',[$.void,['@',$.selector,'@']], function(self, cmd, events) {
-    	self.super('mouseEntered',events);
-    	try {
-    		this.fireEvent('mouseEntered');
-    	} catch(e) { 
-    		console.log(e.message);
-    		console.log(e.stack);
-    		process.exit(1);
-    	}
+      self.super('mouseEntered',events);
+      try {
+        this.fireEvent('mouseEntered');
+      } catch(e) { 
+        console.log(e.message);
+        console.log(e.stack);
+        process.exit(1);
+      }
     }.bind(this));
     //TODO: Does not work.
     nativeViewExt.addMethod('mouseExited:',[$.void,['@',$.selector,'@']], function(self, cmd, events) {
-    	self.super('mouseExited',events);
-    	try { 
-    		this.fireEvent('mouseExited');
-    	} catch(e) { 
-    		console.log(e.message);
-    		console.log(e.stack);
-    		process.exit(1);
-    	}
+      self.super('mouseExited',events);
+      try { 
+        this.fireEvent('mouseExited');
+      } catch(e) { 
+        console.log(e.message);
+        console.log(e.stack);
+        process.exit(1);
+      }
     }.bind(this));
     //TODO: Does not work.
     nativeViewExt.addMethod('mouseMoved:',[$.void,['@',$.selector,'@']], function(self, cmd, events) {
-    	self.super('mouseMoved',events);
-    	try {
-    		this.fireEvent('mouseMoved');
-    	} catch(e) { 
-    		console.log(e.message);
-    		console.log(e.stack);
-    		process.exit(1);
-    	}
+      self.super('mouseMoved',events);
+      try {
+        this.fireEvent('mouseMoved');
+      } catch(e) { 
+        console.log(e.message);
+        console.log(e.stack);
+        process.exit(1);
+      }
     }.bind(this));
     nativeViewExt.register();
 
-		Object.defineProperty(this,'boundsOnScreen', {
-			get:function() {
-				var scnBounds = $.NSScreen('mainScreen')('frame');
-				var vwBounds = this.nativeView('bounds');
-				var winBounds = this.nativeView('convertRect', vwBounds, 'toView', null);
-				var bounds = this.nativeView('window')('convertRectToScreen',winBounds);
-				return {
+    Object.defineProperty(this,'boundsOnScreen', {
+      get:function() {
+        var scnBounds = $.NSScreen('mainScreen')('frame');
+        var vwBounds = this.nativeView('bounds');
+        var winBounds = this.nativeView('convertRect', vwBounds, 'toView', null);
+        var bounds = this.nativeView('window')('convertRectToScreen',winBounds);
+        return {
           x:bounds.origin.x,
           y:(scnBounds.size.height - bounds.origin.y) - vwBounds.size.height,
           width:bounds.size.width,
           height:bounds.size.height
         };
-			}
-		});
+      }
+    });
 
-		Object.defineProperty(this,'boundsOnWindow', {
-			get:function() {
-				var vwBounds = this.nativeView('bounds');
-				var bounds = this.nativeView('convertRect', vwBounds, 'toView', null);
-				return {
+    Object.defineProperty(this,'boundsOnWindow', {
+      get:function() {
+        var vwBounds = this.nativeView('bounds');
+        var bounds = this.nativeView('convertRect', vwBounds, 'toView', null);
+        return {
           x:bounds.origin.x, 
           y:bounds.origin.y - vwBounds.size.height, 
           width:bounds.size.width, 
           height:bounds.size.height
         };
-			}
-		});
+      }
+    });
 
-		Object.defineProperty(this,'bounds',{
-			get:function() {
-				var bounds = this.nativeView('bounds');
-				return {x:bounds.origin.x, y:bounds.origin.y, width:bounds.size.width, height:bounds.size.height};
-			}
-		});
+    Object.defineProperty(this,'bounds',{
+      get:function() {
+        var bounds = this.nativeView('bounds');
+        return {x:bounds.origin.x, y:bounds.origin.y, width:bounds.size.width, height:bounds.size.height};
+      }
+    });
 
-		Object.defineProperty(this, 'nativeClass', { get:function() { return NativeObjectClass; } });
-		Object.defineProperty(this, 'nativeViewClass', { get:function() { return nativeViewExt; } });
-		
-		Object.defineProperty(this, 'native', {
-			get:function() { return native; },
-			set:function(e) { native = e; }
-		});
+    Object.defineProperty(this, 'nativeClass', { get:function() { return NativeObjectClass; } });
+    Object.defineProperty(this, 'nativeViewClass', { get:function() { return nativeViewExt; } });
 
-		Object.defineProperty(this, 'nativeView', {
-			get:function() { return nativeView; },
-			set:function(e) { nativeView = e; }
-		});
+    Object.defineProperty(this, 'native', {
+      get:function() { return native; },
+      set:function(e) { native = e; }
+    });
 
-   Object.defineProperty(this, 'visible', {
-    configurable:true,
-    get:function() { return !this.nativeView('isHidden'); },
-    set:function(e) { return this.nativeView('setHidden',!e); }
-  });
+    Object.defineProperty(this, 'nativeView', {
+      get:function() { return nativeView; },
+      set:function(e) { nativeView = e; }
+    });
+
+    Object.defineProperty(this, 'visible', {
+      configurable:true,
+      get:function() { return !this.nativeView('isHidden'); },
+      set:function(e) { return this.nativeView('setHidden',!e); }
+    });
 
 
  }
