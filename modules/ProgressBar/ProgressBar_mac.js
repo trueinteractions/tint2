7557c52b7c4a32106f8aaaac0c30d54033ebb1b6
Trevor Linton <trevor.linton@gmail.com>
2014-08-23 16:53:54 -0600

    Added ProgressBar

---
+++ modules/ProgressBar/ProgressBar_mac.js
@@ -0,0 +1,64 @@
+module.exports = (function() {
+  var utilities = require('Utilities');
+  var Container = require('Container');
+  var $ = process.bridge.objc;
+
+  function ProgressBar(options) {
+    Container.call(this, $.NSProgressIndicator, $.NSProgressIndicator, {});
+    this.native = this.nativeView = this.nativeViewClass('alloc')('init');
+    this.native('setTranslatesAutoresizingMaskIntoConstraints',$.NO);
+    this.native('setUsesThreadedAnimation',$.YES);
+    this.native('setMinValue', 0);
+    this.native('setMaxValue', 1);
+    this.native('setDoubleValue', 0);
+
+    Object.defineProperty(this, 'border', {
+      get:function() { return this.native('bezeled') ? true : false; },
+      set:function(e) { this.native('setBezeled', e ? true : false); }
+    });
+
+    Object.defineProperty(this, 'value', {
+      get:function() { return this.native('doubleValue'); },
+      set:function(e) { this.native('setDoubleValue', e); }
+    });
+
+    Object.defineProperty(this, 'indeterminate', {
+      get:function() { return this.native('isIndeterminate') ? true : false; },
+      set:function(e) { 
+        this.native('setIndeterminate', e ? true : false);
+        this.native( e ? 'startAnimation' : 'stopAnimation', this.native);
+        this.native('setStyle', e ? $.NSProgressIndicatorSpinningStyle : $.NSProgressIndicatorBarStyle);
+      }
+    });
+
+    Object.defineProperty(this, 'size', {
+      get:function() { 
+        var s = this.native('controlSize');
+        if (s == $.NSMiniControlSize) return "small";
+        else if (s == $.NSRegularControlSize) return "large";
+        else return "normal";
+      },
+      set:function(e) { 
+        if (e == "small") this.native('setControlSize', $.NSMiniControlSize);
+        else if (e == "large") this.native('setControlSize', $.NSRegularControlSize);
+        else this.native('setControlSize', $.NSSmallControlSize);
+      }
+    });
+
+     Object.defineProperty(this, 'style', {
+      get:function() { 
+        var s = this.native('style');
+        if (s == $.NSProgressIndicatorBarStyle) return "bar";
+        else if (s == $.NSProgressIndicatorSpinningStyle) return "spinning";
+      },
+      set:function(e) { 
+        if (e == "bar") this.native('setStyle', $.NSProgressIndicatorBarStyle);
+        else if (e == "spinning") this.native('setStyle', $.NSProgressIndicatorSpinningStyle);
+      }
+    });
+  }
+  ProgressBar.prototype = Object.create(Container.prototype);
+  ProgressBar.prototype.constructor = ProgressBar;
+
+  return ProgressBar;
+})();
---
+++ test/progressbar.js
@@ -0,0 +1,79 @@
+
+/**
+ * @unit-test-setup
+ * @ignore
+ */
+function setup() {
+  global.Window = require('Window');
+  global.ProgressBar = require('ProgressBar');
+}
+
+function baseline() {
+}
+
+/**
+ * @see {Notification}
+ * @example
+ */
+function run($utils) {
+  /* @hidden */ count = 0;
+  var win = new Window();
+  var progress = new ProgressBar();
+  var interval;
+  progress.size = "large";
+  progress.indeterminate = false;
+  //progress.border = true;
+
+  setTimeout(function() {
+    interval = setInterval(function() { 
+      progress.value = progress.value + 1/60;
+      if(progress.value >= 1) {
+        clearInterval(interval);
+        $utils.ok();
+      }
+    }, 1000/60);
+
+  },1000);
+
+  win.appendChild(progress);
+  win.addLayoutConstraint({
+    priority:'required', relationship:'=',
+    firstItem:progress, firstAttribute:'top',
+    secondItem:win, secondAttribute:'bottom',
+    multiplier:0.0, constant:0.0
+  });
+  win.addLayoutConstraint({
+    priority:'required', relationship:'=',
+    firstItem:progress, firstAttribute:'left',
+    secondItem:win, secondAttribute:'left',
+    multiplier:1.0, constant:0.0
+  });
+  win.addLayoutConstraint({
+    priority:'required', relationship:'=',
+    firstItem:progress, firstAttribute:'right',
+    secondItem:win, secondAttribute:'right',
+    multiplier:1.0, constant:0.0
+  });
+
+  win.addLayoutConstraint({
+    priority:'required', relationship:'=',
+    firstItem:progress, firstAttribute:'bottom',
+    secondItem:win, secondAttribute:'bottom',
+    multiplier:0.0, constant:20.0
+  });
+}
+
+/**
+ * @unit-test-shutdown
+ * @ignore
+ */
+function shutdown() {
+}
+
+module.exports = {
+  setup:setup, 
+  run:run, 
+  shutdown:shutdown, 
+  shell:false,
+  name:"ProgressBar",
+};
