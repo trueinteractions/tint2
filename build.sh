eabdb184605f364df0646c1faaeadb65d0ed0371
Trevor Linton <trevor.linton@gmail.com>
2014-08-26 00:28:09 -0600

    updated build/tests to support Xcode and ninja

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
-
-xcodebuild -configuration Release -project build/tint.xcodeproj build
-
+if [ "$(uname)" == "Darwin" ]; then
+  if [ "$(which xcodebuild)" == "" ]; then
+    if [ "$(which ninja)" == ""]; then
+      echo "You do not have Xcode or ninja installed. One of these is required."
+    else
+      ninja -v -C build/out/Release
+    fi
+  else
+    xcodebuild -configuration Release -project build/tint.xcodeproj build    
+  fi
+else
+  echo "Your platform is unfortunately not supported right now."
+fi
--- test/run.sh
+++ test/run.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
+if [ ! -f "../build/Release/tint" ]; then
+  if [ ! -f "../build/out/Release/tint" ]; then
+    echo "Cannot find the binary for tint, exiting."
+    exit 0
+  else
+    TINTEXEC="../build/out/Release/tint"
+  fi
+else
+  TINTEXEC="../build/Release/tint"
+fi
+
 echo
 for file in $@; do
-  ../build/Release/tint tools/utilities.js $file
+  $TINTEXEC tools/utilities.js $file
   test $? -eq 0 || exit $?
 done
 echo
