f93f694fefae8255981ae0a2ae5946870810bd67
Trevor Linton <trevor.linton@gmail.com>
2014-09-03 13:26:37 -0600

    Updated build scripts, beginning win/linux/ios/and

--- config.sh
+++ config.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-if [ "$(uname)" == "Darwin" ]; then
-        GYP_GENERATORS=xcode
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-        GYP_GENERATORS=make
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-        GYP_GENERATORS=msvs
-fi
 
 if [ ! -f "./libraries/node/config.gypi" ]; then
   git submodule init
@@ -15,7 +8,22 @@ if [ ! -f "./libraries/node/config.gypi" ]; then
   cd ../..
 fi
 
-# -Dtarget_arch=x64 
-# -D gyp_output_dir=./build/xcode/
-./tools/gyp/gyp tint.gyp -f xcode -D target_arch=x64  --generator-output=./build/xcode/ --depth=. -I./build/config.gypi -I./build/common.gypi
-./tools/gyp/gyp tint.gyp -f ninja -D target_arch=x64 --generator-output=./build/ninja/ --depth=$PWD -I./build/config.gypi -I./build/common.gypi
+if [ "$(uname)" == "Darwin" ]; then
+  GYP_GENERATORS=xcode,ninja
+  #./tools/gyp/gyp tint.gyp -f ninja -D android=true -D target_arch=and --generator-output=./build/android/ --depth=. -I./build/config.gyp -I./build/common.gypi
+  #./tools/gyp/gyp tint.gyp -f xcode -D arm_emu=true -D target_arch=ios --generator-output=./build/xcode-ios/ --depth=. -I./build/config.gyp -I./build/common.gypi
+  ./tools/gyp/gyp tint.gyp -f xcode -D target_arch=x64  --generator-output=./build/xcode/ --depth=. -I./build/config.gypi -I./build/common.gypi
+  ./tools/gyp/gyp tint.gyp -f ninja -D target_arch=x64 --generator-output=./build/ninja/ --depth=$PWD -I./build/config.gypi -I./build/common.gypi
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  GYP_GENERATORS=make,ninja
+  #./tools/gyp/gyp tint.gyp -f ninja -D android=true -D target_arch=and --generator-output=./build/android/ --depth=. -I./build/config.gyp -I./build/common.gypi
+  ./tools/gyp/gyp tint.gyp -f make -D target_arch=x64  --generator-output=./build/make/ --depth=. -I./build/config.gypi -I./build/common.gypi
+  ./tools/gyp/gyp tint.gyp -f ninja -D target_arch=x64 --generator-output=./build/ninja/ --depth=$PWD -I./build/config.gypi -I./build/common.gypi
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+  GYP_GENERATORS=msvs,ninja
+  #./tools/gyp/gyp tint.gyp -f ninja -D android=true -D target_arch=and --generator-output=./build/android/ --depth=. -I./build/config.gyp -I./build/common.gypi
+  ./tools/gyp/gyp tint.gyp -f msvs -D target_arch=x64  --generator-output=./build/msvs/ --depth=. -I./build/config.gypi -I./build/common.gypi
+  ./tools/gyp/gyp tint.gyp -f ninja -D target_arch=x64 --generator-output=./build/ninja/ --depth=$PWD -I./build/config.gypi -I./build/common.gypi
+else
+  echo "Unfortunately your build platform is not supported."
+fi
