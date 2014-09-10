1ed39d10659b602ec6c207845034825ee951daab
Nathan Wells <nwwells@gmail.com>
2014-09-10 10:55:56 -0600

    removing imageOff method on StatusBar and fixing #46

--- modules/StatusBar/StatusBar_mac.js
+++ modules/StatusBar/StatusBar_mac.js
@@ -57,16 +57,7 @@ module.exports = (function() {
     set:function(e) { 
       this.private.imgOn = e; 
       e = utilities.makeNSImage(e);
-      if(e) this.native('setOnStateImage', e);
-    }
-  });
-
-  Object.defineProperty(StatusBar.prototype, 'imageOff', {
-    get:function() { return this.private.imgOff; },
-    set:function(e) { 
-      this.private.imgOff = e; 
-      e = utilities.makeNSImage(e);
-      if(e) this.native('setOffStateImage', e);
+      if(e) this.native('setAlternateImage', e);
     }
   });
 
