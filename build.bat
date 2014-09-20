d5f5fc36c2bbd01103fe17c71ed92be2b31a5304
trevorlinton <trevor.linton+github@gmail.com>
2014-09-20 00:55:01 -0600

    Another appveyor test...

--- build.bat
+++ build.bat
@@ -1,13 +1,12 @@
 @echo off
 set arg1=%1
 
-reg.exe query "HKLM\SOFTWARE\Microsoft\MSBuild\ToolsVersions\4.0" /v MSBuildToolsPath > nul 2>&1
+reg.exe query "HKLM\SOFTWARE\Microsoft\MSBuild\ToolsVersions\12.0" /v MSBuildToolsPath > nul 2>&1
 if ERRORLEVEL 1 goto MissingMSBuildRegistry
 
-for /f "skip=2 tokens=2,*" %%A in ('reg.exe query "HKLM\SOFTWARE\Microsoft\MSBuild\ToolsVersions\4.0" /v MSBuildToolsPath') do SET MSBUILDDIR=%%B
+for /f "skip=2 tokens=2,*" %%A in ('reg.exe query "HKLM\SOFTWARE\Microsoft\MSBuild\ToolsVersions\12.0" /v MSBuildToolsPath') do SET MSBUILDDIR=%%B
 
-IF NOT EXIST %MSBUILDDIR%nul goto MissingMSBuildToolsPath
-IF NOT EXIST %MSBUILDDIR%msbuild.exe goto MissingMSBuildExe
+IF NOT EXIST "%MSBUILDDIR%msbuild.exe" goto MissingMSBuildExe
 
 if "%arg1%" == "debug" (
   set CONFIG="Debug"
@@ -39,20 +38,15 @@ if "%arg1%" == "debug" (
 ::) else if defined VS100COMNTOOLS if exist "%VS100COMNTOOLS%\..\..\vc\vcvarsall.bat" (
 :: call "%VS100COMNTOOLS%\VCVarsQueryRegistry.bat"
 ::  SETLOCAL
-    if exist "%VS100COMNTOOLS%\..\..\vc\vcvarsall.bat" (
-      call "%VS100COMNTOOLS%\..\..\vc\vcvarsall.bat"
-    )
-    if exist "%VS110COMNTOOLS%\..\..\vc\vcvarsall.bat" (
-      call "%VS110COMNTOOLS%\..\..\vc\vcvarsall.bat"
-    )
-    if exist "%VS120COMNTOOLS%\..\..\vc\vcvarsall.bat" (
-      call "%VS120COMNTOOLS%\..\..\vc\vcvarsall.bat"
-    )
+    :: if exist "%VS120COMNTOOLS%\..\..\vc\vcvarsall.bat" (
+    ::  call "%VS120COMNTOOLS%\..\..\vc\vcvarsall.bat"
+    :: )
 ::      echo "Visual Studio 2013"
     copy /Y tools\v8_js2c_fix.py libraries\node\deps\v8\tools\js2c.py > nul
 ::      call "%VS120COMNTOOLS%\..\..\vc\vcvarsall.bat" x64
 ::      echo "Windows SDK: %WindowsSdkDir%"
-    "%MSBUILDDIR%msbuild.exe" /m /P:Configuration=%CONFIG% /clp:NoSummary;NoItemAndPropertyList;ShowCommandLine; /verbosity:minimal /target:tint /nologo build\msvs\tint.sln 
+::    /p:VisualStudioVersion=12.0 
+    "%MSBUILDDIR%msbuild.exe" /m /P:Configuration=%CONFIG%/clp:NoSummary;NoItemAndPropertyList;ShowCommandLine; /verbosity:minimal /target:tint /nologo build\msvs\tint.sln 
 ::    ) else if exist "%VS110COMNTOOLS%\..\..\vc\vcvarsall.bat" (
 ::      echo "Visual Studio 2012"
 ::      copy /Y tools\v8_js2c_fix.py libraries\node\deps\v8\tools\js2c.py > nul
