ff00f69b5fa840602703188d8048647469217729
Trevor Linton <trevor.linton@gmail.com>
2014-10-21 19:52:28 -0600

    Support for menu unit tests [win]

--- test/menu.js
+++ test/menu.js
@@ -17,6 +17,7 @@ function baseline() {
  * @example
  */
 function run($utils) {
+  /* @hidden */ var ismac = require('os').platform().toLowerCase() == "darwin";
   application.name = "My Program";
   var win = new Window();
   win.visible = true;
@@ -27,6 +28,7 @@ function run($utils) {
   var editMenu = new MenuItem('Edit', '');
   var windowMenu = new MenuItem('Window', '');
   var helpMenu = new MenuItem('Help', '');
+  /* @hidden */ if(ismac)
   mainMenu.appendChild(appleMenu);
   mainMenu.appendChild(fileMenu);
   mainMenu.appendChild(editMenu);
@@ -46,8 +48,8 @@ function run($utils) {
   appleSubmenu.appendChild(new MenuItemSeparator());
   appleSubmenu.appendChild(new MenuItem('Quit '+application.name, 'q'))
       .addEventListener('click', function() { 
-        /* @hidden */ $utils.ok();
-        process.exit(0); 
+        /* @hidden */ if(ismac) $utils.ok();
+        /* @hidden */ if(ismac) process.exit(0); 
       });
   appleMenu.submenu = appleSubmenu;
 
@@ -57,7 +59,11 @@ function run($utils) {
   fileSubmenu.appendChild(new MenuItem('Save', 's'));
   fileSubmenu.appendChild(new MenuItem('Save As...', 'S', 'shift'));
   fileSubmenu.appendChild(new MenuItemSeparator());
-  fileSubmenu.appendChild(new MenuItem('Close', 'c', 'cmd'));
+  fileSubmenu.appendChild(new MenuItem('Close', 'c', 'cmd'))
+    .addEventListener('click', function() {
+      /* @hidden */ if(!ismac) $utils.ok();
+      /* @hidden */ if(!ismac) process.exit(0);
+    });
   fileMenu.submenu = fileSubmenu;
 
   var editSubmenu = new Menu('Edit');
@@ -97,8 +103,22 @@ function run($utils) {
 
   win.menu = mainMenu;
   
-  /* @hidden */ setTimeout(function() { $utils.clickAt(65,15); },1000); 
-  /* @hidden */ setTimeout(function() { $utils.clickAt(65,135); },2000); 
+  /* @hidden */ setTimeout(function() { 
+  /* @hidden */   if(ismac) $utils.clickAt(65,15);
+  /* @hidden */   else {
+  /* @hidden */     var x = win.x + 15;
+  /* @hidden */     var y = win.y + 40;
+  /* @hidden */     $utils.clickAt(x,y);
+  /* @hidden */   }
+  /* @hidden */ },1000); 
+  /* @hidden */ setTimeout(function() { 
+  /* @hidden */   if(ismac) $utils.clickAt(65,135); 
+  /* @hidden */   else {
+  /* @hidden */     var x = win.x + 25;
+  /* @hidden */     var y = win.y + 165;
+  /* @hidden */     $utils.clickAt(x,y);
+  /* @hidden */   }
+  /* @hidden */ },2000); 
 
 }
 
