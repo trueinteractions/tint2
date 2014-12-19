fdee0c1230668c5dd1c7f159369ebe8c4efa623a
Trevor Linton <trevor.linton@gmail.com>
2014-12-19 12:43:32 -0700

    Added more sophisticated box unit test.

--- test/box.js
+++ test/box.js
@@ -28,13 +28,27 @@ function run($utils) {
   var text = new TextInput();
   text.value = "Visual check: lime green bg, 2px red border, 13px corner radius.";
   box.appendChild(text);
-  text.left = '20px';
-  text.top = '150px'
+  text.middle = 0;
+  text.center = 0;
   text.width = 300;
   text.readonly = true;
   setTimeout(function() {
+
+    var bgColor = box.backgroundColor;
+    $utils.assert(bgColor.alpha === 1);
+    $utils.assert(bgColor.red === 0);
+    $utils.assert(bgColor.blue === 0);
+    $utils.assert(bgColor.green === 1);
+
+    var borderColor = box.borderColor;
+    $utils.assert(borderColor.alpha === 1);
+    $utils.assert(borderColor.red === 1);
+    $utils.assert(borderColor.blue === 0);
+    $utils.assert(borderColor.green === 0);
+
     $utils.assert(box.borderRadius == 13);
     $utils.assert(box.borderWidth == 2);
+
     $utils.ok();
   },2000);
 }
