acedeee5c1229e571462496be02b12b5cd141ddc
Trevor Linton <trevor.linton@gmail.com>
2014-12-27 12:48:34 -0700

    Fixed app schema js lint errors.

--- modules/AppSchema/AppSchema_mac.js
+++ modules/AppSchema/AppSchema_mac.js
@@ -1,22 +1,24 @@
 module.exports = function(basepath) {
     var $ = process.bridge.objc;
     var workingdir = process.cwd();
-    if(workingdir[workingdir.length-1] != '/')
+    if(workingdir[workingdir.length-1] !== '/') {
         workingdir += '/';
+    }
    
     var fs = require('fs');
     global.requireNode = global.require;
     global.require = function() {
-        var n = arguments[0];
+      var n = arguments[0];
 
-        //if(application.packaged) {
-            // TODO: ...
-        //} else {
-            if(n[0] == '/') n = n.substring(1);
-            var possible = workingdir + n;
-            if(fs.existsSync(possible)) requireNode(possible);
-            else return global.requireNode.apply(null,arguments);
-        //}
+      if(n[0] === '/') {
+        n = n.substring(1);
+      }
+      var possible = workingdir + n;
+      if(fs.existsSync(possible)) {
+       requireNode(possible);
+     } else {
+        return global.requireNode.apply(null,arguments);
+      }
     }
     global.require.__proto__ = global.requireNode;
 }
