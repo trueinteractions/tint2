e34f3a6a4568ec0aa8bb531ac02c9dffd3c4cec3
Trevor Linton <trevor.linton@gmail.com>
2014-12-28 00:44:39 -0700

    Fixed lint error in Button [win]

--- modules/Button/Button_win.js
+++ modules/Button/Button_win.js
@@ -9,7 +9,6 @@ module.exports = (function() {
 
   function Button(options) {
     options = options || {};
-  
     this.nativeClass = this.nativeClass || $.System.Windows.Controls.Button;
     this.nativeViewClass = this.nativeViewClass || $.System.Windows.Controls.Button;
     Container.call(this, options);
@@ -23,10 +22,13 @@ module.exports = (function() {
 
     this.private.remapNaturalStates = function() {
       this.native.Content = this.private.stack;
-      if(this.type == "normal" || this.type == "toggle")
+
+      if(this.type === "normal" || this.type === "toggle") {
         this.native.Padding = new $.System.Windows.Thickness(5.75,1.75,5.75,1.75);
-      else
+      } else {
         this.native.Padding = new $.System.Windows.Thickness(0,0,0,0);
+      }
+
       // convert pxl based measure to point (18/17) * PixelSize
       this.native.FontSize = this.native.FontSize * 1.05882352941176;
       this.private.defaultBorder = this.nativeView.BorderThickness;
@@ -40,11 +42,14 @@ module.exports = (function() {
 
   Object.defineProperty(Button.prototype, 'border', {
     get:function() { 
-      if(this.private.defaultBorder == this.nativeView.BorderThickness) return true;
-      else return false;
+      if(this.private.defaultBorder === this.nativeView.BorderThickness) {
+        return true;
+      } else { 
+        return false;
+      }
     },
     set:function(e) {
-      this.private.states['border'] = e;
+      this.private.states.border = e;
       if(e) {
         this.nativeView.BorderThickness = this.private.defaultBorder;
         this.nativeView.BorderBrush = this.private.defaultBorderColor;
@@ -57,28 +62,32 @@ module.exports = (function() {
 
   Object.defineProperty(Button.prototype, 'state', {
     get:function() {
-      return typeof(this.nativeView.IsChecked) != 'undefined' ? this.nativeView.IsChecked : false;  
+      return typeof(this.nativeView.IsChecked) !== 'undefined' ? this.nativeView.IsChecked : false;  
     },
     set:function(e) { 
-      this.private.states['state'] = e;
-      if(typeof(this.nativeView.IsChecked) != 'undefined')
+      this.private.states.state = e;
+      if(typeof(this.nativeView.IsChecked) !== 'undefined') {
         this.nativeView.IsChecked = e ? true : false;
+      }
     }
   });
 
   Object.defineProperty(Button.prototype, 'title', {
     get:function() { 
-      if(this.private.label == null) return ""; 
-      else return this.private.label.Content.toString(); 
+      if(this.private.label === null) {
+        return ""; 
+      } else {
+        return this.private.label.Content.toString(); 
+      }
     },
     set:function(e) { 
       this.private.states['title'] = e;
-      if(this.private.label != null)
+      if(this.private.label !== null)
       {
         this.private.stack.InternalChildren.Remove(this.private.label);
         this.private.label = null;
       }
-      if(e && e != null) {
+      if(e && e !== null) {
         this.private.label = new $.System.Windows.Controls.Label();
         this.private.stack.InternalChildren.Add(this.private.label);
         this.private.label.Content = e.toString(); 
@@ -87,29 +96,34 @@ module.exports = (function() {
   });
 
   Object.defineProperty(Button.prototype, 'type', {
-    enumerable:true,
-    configurable:true,
     get:function() { return this.private.buttonType; },
     set:function(type) {
-      this.private.states['type'] = type;
-      
-      if(this.private.buttonType == type) 
+      this.private.states.type = type;
+      if(this.private.buttonType === type) {
         return;
+      }
       
       this.private.buttonType = type;
 
       var oldNative = this.nativeView;
       var parent = this.native.Parent;
-      if(parent != null)
+      if(parent !== null) {
         this.native.Parent.InternalChildren.Remove(this.native);
+      }
 
-      if (type == "toggle") this.nativeView = this.native = new $.System.Windows.Controls.Primitives.ToggleButton();
-      else if (type == "checkbox") this.nativeView = this.native = new $.System.Windows.Controls.CheckBox();
-      else if (type == "radio") this.nativeView = this.native = new $.System.Windows.Controls.RadioButton();
-      else this.nativeView = this.native = new $.System.Windows.Controls.Button();
+      if (type === "toggle") {
+        this.nativeView = this.native = new $.System.Windows.Controls.Primitives.ToggleButton();
+      } else if (type === "checkbox") {
+        this.nativeView = this.native = new $.System.Windows.Controls.CheckBox();
+      } else if (type === "radio") {
+        this.nativeView = this.native = new $.System.Windows.Controls.RadioButton();
+      } else {
+        this.nativeView = this.native = new $.System.Windows.Controls.Button();
+      }
 
-      if(parent != null)
+      if(parent !== null) {
         parent.InternalChildren.Add(this.nativeView);
+      }
 
       this.private.remapNaturalStates();
       this.private.remapStates(oldNative);
@@ -117,12 +131,9 @@ module.exports = (function() {
   });
 
   Object.defineProperty(Button.prototype, 'style', {
-    get:function() { return this.private.states['style']; },
+    get:function() { return this.private.states.style; },
     set:function(type) {
-      this.private.states['style'] = type;
-      if(type == "help") {
-        
-      }
+      this.private.states.style = type;
       /*this.private.buttonStyle = type;
       if(type == "normal") this.nativeView('setBezelStyle',$.NSTexturedRoundedBezelStyle);
       else if (type == "rounded") this.nativeView('setBezelStyle',$.NSRoundedBezelStyle);
@@ -137,29 +148,31 @@ module.exports = (function() {
 
   //TODO: IMPLEMENT ME
   Object.defineProperty(Button.prototype, 'showBorderOnHover', {
-    get:function() { return this.private.states['showBorderOnHover']; },
+    get:function() { return this.private.states.showBorderOnHover; },
     set:function(e) { 
-      this.private.states['showBorderOnHover'] = e ? true : false;
+      this.private.statesshowBorderOnHover = e ? true : false;
     }
   });
 
   Object.defineProperty(Button.prototype, 'enabled', {
     get:function() { return this.IsEnabled ? true : false; },
     set:function(e) {
-      this.private.states['enabled'] = e;
+      this.private.states.enabled = e;
       this.native.IsEnabled = e ? true : false; 
     }
   });
 
   Object.defineProperty(Button.prototype, 'image', {
-    get:function() { return this.private.states['image']; },
+    get:function() { return this.private.states.image; },
     set:function(e) { 
-      this.private.states['image'] = e;
-      if(this.private.img != null)
+      this.private.states.image = e;
+      if(this.private.img !== null) {
         this.private.stack.Children.Remove(this.private.img);
+      }
       this.private.img = utilities.makeImage(e);
-      if(this.private.img != null)
+      if(this.private.img !== null) {
         this.private.stack.Children.Insert(0,this.private.img);
+      }
     }
   });
 
