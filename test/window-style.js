7ddd7cab1610f55b191b7505e8a046027fbaf0f2
Trevor Linton <trevor.linton@gmail.com>
2014-12-28 17:18:14 -0700

    Fixed window style to remove transparency.

--- test/window-style.js
+++ test/window-style.js
@@ -24,13 +24,13 @@ function run($utils) {
   webview.location = "http://www.reddit.com/r/javascript";
   mainWindow.title = "You should be able to see this.";
   var interval = setInterval(function() {
-    mainWindow.backgroundColor = 'rgba(1,0,0,'+alpha+');';
+    mainWindow.backgroundColor = 'rgba('+alpha*255+',0,0,1);';
     alpha = alpha - 30/1000;
     if(alpha <= 0) { 
       alpha = 0;
       clearInterval(interval);
       interval = setInterval(function() {
-        mainWindow.backgroundColor = 'rgba(1,0,0,'+alpha+');';
+        mainWindow.backgroundColor = 'rgba('+alpha*255+',0,0,1);';
         alpha = alpha + 30/1000;
         if(alpha >= 1) {
           clearInterval(interval);
