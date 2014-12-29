bdbec23054d1194b3d8d70538c255e8a9ca6be2c
Trevor Linton <trevor.linton@gmail.com>
2014-12-29 11:54:01 -0700

    Fixed webview on windows, removed specs
    * added new-window event for windows.\
    * standardized some behaviors between webview on windows and mac.

--- modules/WebView/WebView_win.js
+++ modules/WebView/WebView_win.js
@@ -1,99 +1,122 @@
 module.exports = (function() {
+
   if(global.__TINT.WebView) {
     return global.__TINT.WebView;
   }
 
   var Container = require('Container');
   var $ = process.bridge.dotnet;
+  $.import('System.Windows.Forms.dll');
+  $.import('WPF\\WindowsFormsIntegration.dll');
 
   function WebView(options) {
     options = options || {};
-    this.nativeClass = this.nativeClass || $.System.Windows.Controls.WebBrowser;
-    this.nativeViewClass = this.nativeViewClass || $.System.Windows.Controls.WebBrowser;
+    options.nonStandardEvents = true;
+    this.nativeClass = this.nativeClass || $.System.Windows.Forms.WebBrowser;
+    this.nativeViewClass = this.nativeViewClass || $.System.Windows.Forms.WebBrowser;
+
     Container.call(this, options);
 
-    var firstLoad = true;
+    this.private.webview = this.nativeView;
+    this.native = this.nativeView = new $.System.Windows.Forms.Integration.WindowsFormsHost();
+    this.native.Child = this.private.webview;
+    this.private.progress = -1;
     this.private.previousTitle = "";
     this.private.loading = false;
     this.private.useragent = "";
 
     this.private.checkForNewTitle = function() {
-      var currentTitle = this.nativeView.InvokeScript("eval",['document.title']);
+      var currentTitle = this.private.webview.Document.InvokeScript("eval",['document.title']);
       if(this.private.previousTitle !== currentTitle) {
         this.private.previousTitle = currentTitle;
         this.fireEvent('title');
       }
     }.bind(this);
 
-    this.nativeView.addEventListener('LoadCompleted', function() { 
+    this.private.webview.addEventListener('ProgressChanged', function(E, a) {
+      var z = process.bridge.dotnet.fromPointer(a);
+      if(e.CurrentProgress < 0 || z.MaximumProgress !== 0) {
+        this.private.progress = -1;
+      } else {
+        this.private.progress = z.CurrentProgress / z.MaximumProgress;
+      }
+    });
+
+    this.private.webview.ScriptErrorsSuppressed = true;
+
+    this.private.webview.addEventListener('DocumentCompleted', function() {
       this.private.loading = false;
       setTimeout(function() {
-        if(!this.nativeView.Source) {
+        if(!this.private.webview.Url) {
           this.fireEvent('error');
         } else {
-          this.nativeView.InvokeScript("eval",['window.postMessageToHost = function(e) { window.external.postMessageBack(e); };']);
+          this.private.webview.Document.InvokeScript("eval",['window.postMessageToHost = function(e) { window.external.postMessageBack(e); };']);
           this.fireEvent('title');
           this.fireEvent('load');
         }
       }.bind(this),105);
     }.bind(this));
-    this.nativeView.addEventListener('Navigated', function() { this.fireEvent('loading'); }.bind(this));
-    this.nativeView.addEventListener('Navigating', function() {
-      if(firstLoad) {
-        // establish the activex instance, store a ref for later.
-        this.private.comObject = this.native._axIWebBrowser2;
-        // Use the activeX object to silent error messages.
-        this.private.comObject.GetType().InvokeMember("Silent", $.System.Reflection.BindingFlags.SetProperty, null, this.private.comObject, [ true ], null, null, null);
-        firstLoad = false;
-      } else {
-        this.fireEvent('unload');
-        this.fireEvent('request');
-      }
+
+    this.private.webview.addEventListener('Navigated', function() {
+      this.fireEvent('loading'); 
+    }.bind(this));
+
+    this.private.webview.addEventListener('Navigating', function() {
+      this.fireEvent('unload');
+      this.fireEvent('request');
       this.private.loading = true;
       this.fireEvent('locationchange');
     }.bind(this));
+
+    this.private.webview.addEventListener('DocumentTitleChanged', function() {
+      this.fireEvent('title');
+    });
+
+    this.private.webview.addEventListener('NewWindow', function(a, b) {
+      var y = process.bridge.dotnet.fromPointer(a);
+      var z = process.bridge.dotnet.fromPointer(b);
+      this.fireEvent('new-window');
+      // TODO: determine url, and decide how this should be handled...
+    });
 
     // Callback class for IE to call postMessage;
     function callbackHandle(str) {
       this.fireEvent('message',[str]);
     }
     var scriptInterface = process.bridge.createScriptInterface(callbackHandle.bind(this));
-    this.nativeView.ObjectForScripting = scriptInterface;
+    this.private.webview.ObjectForScripting = scriptInterface;
 
     // TODO: Support events
     // redirect
     // icon
-    // new-window
 
     // If we don't have a document loaded IE will throw an error if anything
     // other than a location is set, so we'll set an intiial location to prevent this.
-    this.nativeView.Navigate(new $.System.Uri("http://127.0.0.1:" + application.private.appSchemaPort + "/this-is-a-blank-page-for-ie-fix.html"));
+    this.private.webview.Navigate(new $.System.Uri("http://127.0.0.1:" + application.private.appSchemaPort + "/this-is-a-blank-page-for-ie-fix.html"));
   }
 
   WebView.prototype = Object.create(Container.prototype);
   WebView.prototype.constructor = Container;
 
-  WebView.prototype.back = function() { this.nativeView.GoBack(); }
-  WebView.prototype.forward = function() { this.nativeView.GoForward(); }
-  WebView.prototype.reload = function() { this.nativeView.Refresh(); }
+  WebView.prototype.back = function() { this.private.webview.GoBack(); }
+  WebView.prototype.forward = function() { this.private.webview.GoForward(); }
+  WebView.prototype.reload = function() { this.private.webview.Refresh(); }
   WebView.prototype.stop = function() { 
     this.private.loading = false;
-    this.private.comObject.GetType().InvokeMember("Stop", $.System.Reflection.BindingFlags.InvokeMember, null, this.private.comObject, null, null, null, null);
+    this.private.webview.Stop();
     this.fireEvent('cancel');
   }
 
   WebView.prototype.postMessage = function(e) {
     var msg = "var msg=document.createEvent('MessageEvent');\n";
-    msg += "msg.initMessageEvent('message',true,true,'" + 
-      e.toString().replace(/'/g,"\\'") + 
-      "','*',0,window);\n";
+    msg += "msg.initMessageEvent('message',true,true,'" + e.toString().replace(/'/g,"\\'") + "','*',0,window);\n";
     msg += "window.dispatchEvent(msg);\n";
     this.execute(msg);
     this.private.checkForNewTitle();
   }
 
   WebView.prototype.execute = function(e) { 
-    return this.nativeView.InvokeScript("eval",[e]);
+    return this.private.webview.Document.InvokeScript("eval",[e]);
   }
 
   Object.defineProperty(WebView.prototype, 'icon', {
@@ -112,7 +135,7 @@ module.exports = (function() {
 
   // Indeterminate on windows.
   Object.defineProperty(WebView.prototype, 'progress', {
-    get:function() { return -1; }
+    get:function() { return this.private.progress; }
   });
 
   Object.defineProperty(WebView.prototype, 'location', {
@@ -122,22 +145,25 @@ module.exports = (function() {
       // Win8 adds (or seemingly adds) new support for URI schemas, investigate url monikers
       // as well, asyncronous pluggable protocols wont work since they're system wide and
       // not app dependent.
-      if(url.indexOf("app:") > -1)// && !application.packaged)
+      if(url.indexOf("app:") > -1) {
         url = url.replace("app:/","http://127.0.0.1:"+application.private.appSchemaPort+"/");
-      this.nativeView.Navigate(new $.System.Uri(url));
+      }
+      this.private.webview.Navigate(new $.System.Uri(url));
     }
   });
 
   Object.defineProperty(WebView.prototype, "useragent", {
     get:function() {
       var userAgent = this.private.useragent;
-      if(!userAgent)
-        userAgent = this.nativeView.InvokeScript("eval",['navigator.userAgent']);
+      if(!userAgent) {
+        userAgent = this.private.webview.Document.InvokeScript("eval",['navigator.userAgent']);
+      }
       return userAgent; 
     },
     set:function(e) { 
-      if(application.warn)
+      if(global.application.warn) {
         console.error('User agent is not yet supported on IE/Windows.');
+      }
       this.private.useragent = e;
     }
   });
