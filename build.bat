1f041d45c1ef74725c7185b6c8609754f27452fc
Trevor Linton <trevor.linton@gmail.com>
2015-01-11 17:52:46 -0700

    Fixed VS2012 fallbacks.

--- build.bat
+++ build.bat
@@ -86,16 +86,16 @@ if defined noperfctr set noperfctr_arg=--without-perfctr& set noperfctr_msi_arg=
 :project-gen
 if defined NIGHTLY set TAG=nightly-%NIGHTLY%
 SETLOCAL
-	if defined VS110COMNTOOLS if exist "%VS110COMNTOOLS%\VCVarsQueryRegistry.bat" (
-		call "%VS110COMNTOOLS%\VCVarsQueryRegistry.bat"
-		set GYP_MSVS_VERSION=2012
-		set platformtoolset=v110
-		goto inner-config
-	)
 	if defined VS100COMNTOOLS if exist "%VS100COMNTOOLS%\VCVarsQueryRegistry.bat" (
 		call "%VS100COMNTOOLS%\VCVarsQueryRegistry.bat"
 		set GYP_MSVS_VERSION=2010
 		set platformtoolset=v100
+		goto inner-config
+	)
+	if defined VS110COMNTOOLS if exist "%VS110COMNTOOLS%\VCVarsQueryRegistry.bat" (
+		call "%VS110COMNTOOLS%\VCVarsQueryRegistry.bat"
+		set GYP_MSVS_VERSION=2012
+		set platformtoolset=v110
 		goto inner-config
 	)
 	echo Cannot find visual studio VCVarsQueryRegistry.bat
@@ -110,18 +110,18 @@ ENDLOCAL
 :msbuild
 if defined nobuild goto sign
 SETLOCAL
-	if defined VS110COMNTOOLS if exist "%VS110COMNTOOLS%\..\..\vc\vcvarsall.bat" (
-		echo Using Platform Toolset V110
-		call "%VS110COMNTOOLS%\..\..\vc\vcvarsall.bat"
-		set GYP_MSVS_VERSION=2012
-		set platformtoolset=v110
-		goto msbuild-found
-	)
 	if defined VS100COMNTOOLS if exist "%VS100COMNTOOLS%\..\..\vc\vcvarsall.bat" (
 		echo Using Platform Toolset V100
 		call "%VS100COMNTOOLS%\..\..\vc\vcvarsall.bat"
 		set GYP_MSVS_VERSION=2010
 		set platformtoolset=v100
+		goto msbuild-found
+	)
+	if defined VS110COMNTOOLS if exist "%VS110COMNTOOLS%\..\..\vc\vcvarsall.bat" (
+		echo Using Platform Toolset V110
+		call "%VS110COMNTOOLS%\..\..\vc\vcvarsall.bat"
+		set GYP_MSVS_VERSION=2012
+		set platformtoolset=v110
 		goto msbuild-found
 	)
 	echo Cannot find vcvarsall.bat for visual studio
