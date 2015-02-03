02b0a54f9e4247502270f40dd85c0e1efca0cb91
Trevor Linton <trevor.linton@gmail.com>
2015-02-02 22:10:56 -0700

    Fixed app schema path/rel logic [osx]
    * Removed require override (this was never a good idea anyway) all app
    schemas go through URL protocol handlers; applied resource loading at
    lower node.js level rather than overriding require.
    * Fixed logic of app schema so it does not rely on just the working
    dir; when in packaged mode it only searches off of the package.json
    relative file.  When in script mode it searches from the base of the
    passed in script, and then the working dir; in interactive mode it
    searches just off the working dir.

--- modules/AppSchema/AppSchema.js
+++ modules/AppSchema/AppSchema.js
@@ -57,25 +57,6 @@ module.exports = (function() {
     server.listen(application.private.appSchemaPort);
   }
 
-  var workingdir = process.cwd();
-  if(workingdir[workingdir.length-1] !== '/') {
-    workingdir += '/';
-  }
-  var fs = require('fs');
-  global.requireNode = global.require;
-  global.require = function() {
-    var n = arguments[0];
-    if(n[0] === '/') {
-      n = n.substring(1);
-    }
-    var possible = workingdir + n;
-    if(fs.existsSync(possible)) {
-      global.requireNode(possible);
-    } else {
-      return global.requireNode.apply(null,arguments);
-    }
-  };
-
   runApplicationHost();
 
   global.__TINT.AppSchema = true;
--- modules/AppSchema/AppSchema_mac.mm
+++ modules/AppSchema/AppSchema_mac.mm
@@ -3,19 +3,67 @@
 @implementation AppSchema
 
 - (void)startLoading {
+	// Remove app:/// protocol.
 	NSString *url = [[[self request] URL] description];
 	if([url rangeOfString:@"app:///"].location != NSNotFound)
 		url = [url stringByReplacingOccurrencesOfString:@"app:///" withString:@"/"];
 	else if([url rangeOfString:@"app://"].location != NSNotFound)
 		url = [url stringByReplacingOccurrencesOfString:@"app://" withString:@"/"];
 
-	NSFileManager *filemgr;
-	NSString *currentpath;
-	filemgr = [NSFileManager defaultManager];
-	currentpath = [filemgr currentDirectoryPath];
-	NSString *possible = [currentpath stringByAppendingString:url];
-	if ([filemgr fileExistsAtPath:possible] == YES) {
-		NSData *data = [filemgr contentsAtPath:possible];
+	NSFileManager *filemgr = [NSFileManager defaultManager];
+	NSBundle *bundle = [NSBundle mainBundle];
+	NSString *package = [bundle pathForResource:@"package" ofType:@"json"];
+	NSString *identifier = [bundle bundleIdentifier];
+	NSString *path = nil;
+	if(package && identifier) {
+		// We're in a packaged context; in this situation we cannot trust
+		// the command line arguments or working directory (to prevent security issues)
+		path = [bundle resourcePath];
+	} else {
+		// We're not bundled, but executed by a command line script, if we have
+		// an argument on argv use that as the path root.
+		NSArray *args = [[NSProcessInfo processInfo] arguments];
+		for(NSUInteger i=1; i < [args count]; i++) {
+			NSString *arg = (NSString *)[args objectAtIndex:i];
+			// if the first character is not '-'
+			if(![[arg substringToIndex:1] isEqualToString:@"-"]) {
+				path = arg;
+				break;
+			}
+		}
+		// we did not find a passed in script, use the working directory.
+		if(path == nil) {
+			path = [filemgr currentDirectoryPath];
+
+		// we did find a passed in script, go ahead and remove the file name and 
+		// keep the path. NSURL will help by resolving relative paths to the 
+		// working directory or preserving absolute ones.
+		} else {
+			NSArray *pathComponents = [[NSURL fileURLWithPath:path] pathComponents];
+			path = @"";
+			for(NSUInteger i=0; i < ([pathComponents count] - 1); i++) {
+				NSString *component = (NSString *)[pathComponents objectAtIndex:i];
+				if(component && ![component isEqualToString:@""] && ![component isEqualToString:@"/"]) {
+					path = [[path  stringByAppendingString:@"/"] stringByAppendingString:component];
+				}
+			}
+		}
+	}
+	// if url has a leading /
+	if([[url substringToIndex:1] isEqualToString:@"/"]) {
+		path = [[path stringByAppendingString:@"/."] stringByAppendingString:url];
+	} else {
+		path = [[path stringByAppendingString:@"/"] stringByAppendingString:url];
+	}
+
+	// one last backup, if we are NOT a packaged app; check the working path one last time.
+	if(!(package && identifier) && ([filemgr fileExistsAtPath:path] == NO)) {
+		path = [filemgr currentDirectoryPath];
+		path = [[path stringByAppendingString:@"/"] stringByAppendingString:url];
+	}
+
+	if ([filemgr fileExistsAtPath:path] == YES) {
+		NSData *data = [filemgr contentsAtPath:path];
 		NSURLResponse *response = [[NSURLResponse alloc] initWithURL:[[self request] URL] MIMEType:@"" expectedContentLength:[data length] textEncodingName:nil];
 		[[self client] URLProtocol:self didReceiveResponse:response cacheStoragePolicy:NSURLCacheStorageAllowed];
 		[[self client] URLProtocol:self didLoadData:data];
