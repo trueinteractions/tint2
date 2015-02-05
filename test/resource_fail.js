a21aa03c2eb5af9840e7d531fcff52c2cdc3bdff
Trevor Linton <trevor.linton@gmail.com>
2015-02-04 19:55:51 -0700

    Fix for resource fail on windows.

--- test/resource_fail.js
+++ test/resource_fail.js
@@ -13,27 +13,46 @@ function baseline() {
 
 function run($utils) {
 	var ismac = require('os').platform().toLowerCase() == "darwin";
-	var spawn = require('child_process').spawn,
-    	resc  = spawn(ismac ? '../build/xcode/Release/tint' : '..\\build\\msvs\\Release\\tint.exe' , 
-        ['../tools/compiler/tntbuild.js','--no-windows-build','--osx-runtime=../build/xcode/Release/tint','--out=./packagetestfail-build/','./packagetestfail/package.json']
-        );
-	resc.on('close', function (code, signal) {
+  if(ismac) {
+  	var spawn = require('child_process').spawn,
+      	resc  = spawn('../build/xcode/Release/tint' , 
+          ['../tools/compiler/tntbuild.js','--no-windows-build','--osx-runtime=../build/xcode/Release/tint','--out=./packagetestfail-build/','./packagetestfail/package.json']
+          );
+  	resc.on('close', function (code, signal) {
   		$utils.assert(code === 1);
-      if(ismac) {
+      
         var rmdir = spawn('rm', ['-rf', './packagetestfail-build/']);
         rmdir.on('close', function(code, signal) {
           $utils.assert(code === 0);
           $utils.ok();
         });
-      } else {
-        var rmdir = spawn('rmdir', ['/S', '.\\packagetestfail-build\\']);
-        rmdir.on('close', function(code, signal) {
-          $utils.assert(code === 0);
-          $utils.ok();
-        });
-      }
+    });
+
+  } else {
+    var spawn = require('child_process').spawn,
+      resc  = spawn('..\\build\\msvs\\Release\\tint.exe' , 
+        ['../tools/compiler/tntbuild.js','--no-osx-build','--windows-runtime=../build/msvs/Release/tint.exe','--out=./packagetestfail-build/','./packagetestfail/package.json']
+        );
+    resc.on('close', function (code, signal) {
+      $utils.assert(code === 1);
+      var rmdir = spawn('rmdir', ['/S', '.\\packagetestfail-build\\']);
+      rmdir.on('close', function(code, signal) {
+        $utils.assert(code === 0);
+        $utils.ok();
+      });
+      rmdir.on('error', function(err) {
+        $utils.ok();
+        console.log('rmdir: ', err);
+      });
+
+    });
+    resc.on('error', function(err) {
+      console.log('resc error');
+      
+    });
+  }
   		
-	});
+	
 }
 
 /**
@@ -48,5 +67,5 @@ module.exports = {
   run:run, 
   shutdown:shutdown, 
   shell:false,
-  name:"ResourcesCommandLine",
+  name:"ResourcesCommandLineFail",
 };
