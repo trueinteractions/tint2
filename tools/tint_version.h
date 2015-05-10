a4fed01a1d10e41470318a6538214813625b133a
Trevor Linton <trevor.linton@gmail.com>
2015-05-10 15:30:14 -0600

    Bump to v2.1.11

--- tools/tint_version.h
+++ tools/tint_version.h
@@ -1,6 +1,6 @@
 #define TINT_MAJOR_VERSION 2
 #define TINT_MINOR_VERSION 1
-#define TINT_PATCH_VERSION 10
+#define TINT_PATCH_VERSION 11
 // #define TINT_TAG ""			// comment out for releases.
 
 #ifndef TINT_STRINGIFY
